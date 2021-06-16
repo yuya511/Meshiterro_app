9825cf04683a0e2e804628f8de98a0cdb54e2665
yuya511 <20015114452y@gmail.com>
2021-06-16 08:50:09 +0000

    Chapter4 finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    #resourceのように単数にすると、コントローラのidがリクエストに含まれなくなる
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  
 end
